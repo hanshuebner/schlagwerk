acec01122731d803f3d73f82d03cb7231ad15bfb
Hans Huebner <hans.huebner@gmail.com>
2015-01-05 20:57:42 +0100

    implement 32 channel pwm

--- schlagwerk.c
+++ schlagwerk.c
@@ -39,6 +39,15 @@ void SetupHardware(void)
   clock_prescale_set(clock_div_1);
 
   USB_Init();
+
+  // Timer 0 konfigurieren
+  TCCR0A = (1<<WGM01); // CTC Modus
+  TCCR0B |= (1<<CS01); // Prescaler 8
+  // ((16000000/8)/100000) = 20 (100khz)
+  OCR0A = 20-1;
+ 
+  // Compare Interrupt erlauben
+  TIMSK0 |= (1<<OCIE0A);
 }
 
 void EVENT_USB_Device_ConfigurationChanged(void)
@@ -51,7 +60,36 @@ void EVENT_USB_Device_ControlRequest(void)
   MIDI_Device_ProcessControlRequest(&Keyboard_MIDI_Interface);
 }
 
-int main(void)
+static uint8_t sample_buffer[128][4];
+static uint8_t sample_pointer = 0;
+
+ISR (TIMER0_COMPA_vect)
+{
+  PORTB = sample_buffer[sample_pointer][0];
+  PORTC = sample_buffer[sample_pointer][1];
+  PORTD = sample_buffer[sample_pointer][2];
+  PORTF = sample_buffer[sample_pointer][3];
+  sample_pointer++;
+}
+
+void
+set_channel(uint8_t channel, uint8_t value)
+{
+  uint8_t byte_index = channel >> 3;
+  uint8_t mask = 1 << (channel & 0x07);
+  for (uint8_t i = 0; i < 128; i++) {
+    if (i < value) {
+      sample_buffer[i][byte_index] |= mask;
+      continue;
+    } else if (i == value) {
+      mask = ~mask;
+    }
+    sample_buffer[i][byte_index] &= mask;
+  }
+}
+
+int
+main(void)
 {
   SetupHardware();
 
@@ -60,12 +98,12 @@ int main(void)
   for (;;) {
     MIDI_EventPacket_t ReceivedMIDIEvent;
     while (MIDI_Device_ReceiveEventPacket(&Keyboard_MIDI_Interface, &ReceivedMIDIEvent)) {
-      if ((ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_ON)) && (ReceivedMIDIEvent.Data3 > 0)) {
-        if (ReceivedMIDIEvent.Data2 > 64) {
-          PORTD = 0xff;
+      if (ReceivedMIDIEvent.Data2 < 32) {
+        if (ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_ON)) {
+          set_channel(ReceivedMIDIEvent.Data2, ReceivedMIDIEvent.Data3);
+        } else if (ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_OFF)) {
+          set_channel(ReceivedMIDIEvent.Data2, 0);
         }
-      } else {
-        PORTD = 0;
       }
     }
 
