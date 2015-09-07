af105f78c4844d7dc5e8b195244762cc35b1ec80
Hans Huebner <hans.huebner@lambdawerk.com>
2015-09-07 14:49:00 +0200

    Simples an/aus zum testen

--- schlagwerk.c
+++ schlagwerk.c
@@ -4,29 +4,9 @@
 #include <avr/wdt.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include <stdbool.h>
 #include <string.h>
-#include "Descriptors.h"
-
-#include <LUFA/Drivers/Board/LEDs.h>
-#include <LUFA/Drivers/USB/USB.h>
-#include <LUFA/Platform/Platform.h>
-
-USB_ClassInfo_MIDI_Device_t Keyboard_MIDI_Interface = {
-  .Config = {
-    .StreamingInterfaceNumber = INTERFACE_ID_AudioStream,
-    .DataINEndpoint           = {
-      .Address          = MIDI_STREAM_IN_EPADDR,
-      .Size             = MIDI_STREAM_EPSIZE,
-      .Banks            = 1,
-    },
-    .DataOUTEndpoint          = {
-      .Address          = MIDI_STREAM_OUT_EPADDR,
-      .Size             = MIDI_STREAM_EPSIZE,
-      .Banks            = 1,
-    }
-  }
-};
 
 /** Configures the board hardware and chip peripherals. */
 void SetupHardware(void)
@@ -38,61 +18,11 @@ void SetupHardware(void)
   /* Disable clock division */
   clock_prescale_set(clock_div_1);
 
-  USB_Init();
-
   // Pins als Outputs definition
   DDRB = 0xff;
   DDRC = 0xff;
   DDRD = 0xff;
   DDRF = 0xff;
-
-  // Timer 0 konfigurieren
-  TCCR0A = (1<<WGM01); // CTC Modus
-  TCCR0B |= (1<<CS01); // Prescaler 8
-  // ((16000000/8)/100000) = 20 (100khz)
-  OCR0A = 20-1;
- 
-  // Compare Interrupt erlauben
-  TIMSK0 |= (1<<OCIE0A);
-}
-
-void EVENT_USB_Device_ConfigurationChanged(void)
-{
-  MIDI_Device_ConfigureEndpoints(&Keyboard_MIDI_Interface);
-}
-
-void EVENT_USB_Device_ControlRequest(void)
-{
-  MIDI_Device_ProcessControlRequest(&Keyboard_MIDI_Interface);
-}
-
-static uint8_t sample_buffer[128][4];
-static uint8_t sample_pointer = 0;
-
-ISR (TIMER0_COMPA_vect)
-{
-  uint8_t* frame = sample_buffer[sample_pointer++];
-  sample_pointer %= 128;
-  PORTB = *frame++;
-  PORTC = *frame++;
-  PORTD = *frame++;
-  PORTF = *frame++;
-}
-
-void
-set_channel(uint8_t channel, uint8_t value)
-{
-  uint8_t byte_index = channel >> 3;
-  uint8_t mask = 1 << (channel & 0x07);
-  for (uint8_t i = 0; i < 128; i++) {
-    if (i < value) {
-      sample_buffer[i][byte_index] |= mask;
-      continue;
-    } else if (i == value) {
-      mask = ~mask;
-    }
-    sample_buffer[i][byte_index] &= mask;
-  }
 }
 
 int
@@ -100,22 +30,17 @@ main(void)
 {
   SetupHardware();
 
-  GlobalInterruptEnable();
-
   for (;;) {
-    MIDI_EventPacket_t ReceivedMIDIEvent;
-    while (MIDI_Device_ReceiveEventPacket(&Keyboard_MIDI_Interface, &ReceivedMIDIEvent)) {
-      if (ReceivedMIDIEvent.Data2 < 32) {
-        if (ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_ON)) {
-          set_channel(ReceivedMIDIEvent.Data2, ReceivedMIDIEvent.Data3);
-        } else if (ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_OFF)) {
-          set_channel(ReceivedMIDIEvent.Data2, 0);
-        }
-      }
-    }
-
-    MIDI_Device_USBTask(&Keyboard_MIDI_Interface);
-    USB_USBTask();
+    PORTB = 0;
+    PORTC = 0;
+    PORTD = 0;
+    PORTF = 0;
+    _delay_ms(1000);
+    PORTB = 255;
+    PORTC = 255;
+    PORTD = 255;
+    PORTF = 255;
+    _delay_ms(1000);
   }
 }
 
